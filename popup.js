584e89f7a4f14135f4f2adb384efbdf46f4b6990
DerPW <34401181+DerPW@users.noreply.github.com>
2020-07-20 22:22:03 +0200

    version tag and update button

--- popup.js
+++ popup.js
@@ -20,3 +20,16 @@ chrome.runtime.onMessage.addListener((msg, sender) => {
 	start.innerText = 'Start';
 	start.disabled = false;
 });
+
+const version = document.getElementById('version');
+
+window.onload = async () => {
+	const res = await fetch(
+		'https://api.github.com/repos/DerPW/TyperacerBot-Extension/releases'
+	).then((res) => {
+		return res.json();
+	});
+	res[0].created_at > res[1].created_at
+		? (version.innerHTML = `<span class="mdl-chip__text">v${res[0].tag_name}</span>`)
+		: (version.innerHTML = `<a class="mdl-chip__text" target="_blank" href='${res[0].html_url}'>Update now (${res[1].tag_name} -> ${res[0].tag_name})</a>`);
+};
